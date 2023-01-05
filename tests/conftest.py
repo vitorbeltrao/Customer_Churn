e1b5826abcfdff015135e4204355f4b21a12d690
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-01-05 18:35:29 -0300

    🥅 Create test files

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+# '''
+# '''
+
+# import pytest
+# from customer_churn_model.etl_workflow.etl import import_data
+
+# # config
+# DATASET = 'C:/Users/4YouSee/Desktop/personal_work/customer_churn/data/bank_data.csv'
+
+# @pytest.fixture()
+# def sample_input_data():
+#     df = import_data(DATASET)
+#     return df
