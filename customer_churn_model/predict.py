fbac22a1e8b0ebb666ded45c6a204b755a51cf31
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-01-08 14:50:05 -0300

    🎨 Finish the predict.py file and general clean up

--- customer_churn_model/predict.py
+++ customer_churn_model/predict.py
@@ -18,43 +18,40 @@ _churn_pipeline = joblib.load(SAVE_PKL_FILE_NAME)
 
 
 def predict_churn(input_data: str) -> dict:
-    '''
-    '''
-    new_df = import_data(input_data)
-    results = {'predictions': None}
+    '''Function to make predictions on new data. To use this function on
+    new data, you must upload your data in the "customer_churn_model/new_data"
+    folder and your file must be called "new_data"
 
-    predictions = _churn_pipeline.predict(new_df)
+    :param input_data: (str)
+    The path of the file that should call "new_data"
 
-    results = {
-        'predictions': predictions,
-    }
-    print('Execution of predict_churn: SUCCESS', results)
-    return results
+    :return: (dict)
+    The prediction of your data
+    '''
+    try:
+        results = {'predictions': None}
+        predictions = _churn_pipeline.predict(import_data(input_data))
+        results = {'predictions': predictions}
+
+        print('The results are:', results)
+        logging.info('Execution of predict_churn: SUCCESS')
+        return results
+
+    except FileNotFoundError:
+        logging.error("Execution of predict_churn: The file wasn't found")
+        return None
+
+    except TypeError:
+        logging.error(
+            'Execution of predict_churn: Your input is not correctly')
+        return None
 
 
 if __name__ == "__main__":
     logging.info('About to start the prediction')
+    print('predicting on new data...')
 
-    try:
-        raw_df = import_data(NEW_DATA)
-        print('predicting on new data...')
+    predict_churn(NEW_DATA)
 
-        X_test = raw_df.copy()
-        predict_churn(X_test)
-
-        logging.info('Done executing the predictions: SUCCESS')
-
-    except FileNotFoundError:
-        print("Execution of predict_churn: The file wasn't found")
-        logging.error("Execution of predict_churn: The file wasn't found")
-
-    except TypeError:
-        print('Execution of predict_churn: Your input is not correctly')
-        logging.error('Execution of predict_churn: Your input is not correctly')
-
-
-
-
-
-
-    
+    print('The predict_churn function has been executed: Executed System!')
+    logging.info('Done executing the predictions: SUCCESS')
